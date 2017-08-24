6e5901f7696183f77c766b1c1dc0282b409ce538
Evan Gui <evan176@hotmail.com>
2017-08-24 11:45:10 +0800

    Change name in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="",
+    name="rl",
     version="0.0.1",
     author="Evan Gui",
     author_email="evan176.gui@gmail.com",
